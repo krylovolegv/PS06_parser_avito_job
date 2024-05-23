cb0b1fce2e448830e0147d468609e08a08789a00
krylovolegv <krylovolegv@yandex.ru>
2024-05-23 16:18:32 +0200

    add code 4 simple pars

--- master.py
+++ master.py
@@ -20,16 +20,29 @@ url = "https://www.avito.ru/kaliningrad/vakansii/iurist-ASgBAgICAUTUzBHo7YYD"
 # Открываем веб-страницу
 driver.get(url)
 
-# Задаём 10 секунд ожидания, чтобы веб-страница успела прогрузиться
+# Задаём 5 секунд ожидания, чтобы веб-страница успела прогрузиться
+time.sleep(5)
+
+# Перезагружаем веб-страницу
+driver.refresh()
+
+# Задаём 10 секунд ожидания, чтобы веб-страница успела прогрузиться после перезагрузки
 time.sleep(10)
 
 # Находим все карточки с вакансиями с помощью названия класса
 # Названия классов берём с кода сайта
 vacancies = driver.find_elements(By.CLASS_NAME, 'iva-item-root-_lk9K')
 
-# Выводим вакансии на экран
-for vacancy in vacancies:
-    print(vacancy.text)
+# Проверяем, что вакансии найдены
+if vacancies:
+    print(f"Найдено вакансий: {len(vacancies)}")
+    # Выводим вакансии на экран
+    # for vacancy in vacancies:
+    #     title_element = vacancy.find_element(By.CLASS_NAME, 'styles-module-root-GKtmM')  # Обновите селектор, если необходимо
+    #     title = title_element.text if title_element else "Не удалось получить название"
+    #     print(f"Вакансия: {title}")
+else:
+    print("Вакансий не найдено.")
 
 # Закрываем браузер
 driver.quit()
